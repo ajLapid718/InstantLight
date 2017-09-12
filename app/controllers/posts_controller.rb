780d02c58a18545b18bfa63f821baf5f6156a11f
Allan James Lapid <ajLapid718@gmail.com>
2017-09-12 12:26:22 -0400

    Be more specific with posts_path instead of root_path in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to root_path
+      redirect_to posts_path
     else
       render :new
     end
