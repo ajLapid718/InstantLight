27f78c6a2111e95e276a13aab467d585063a68f5
Allan James Lapid <ajLapid718@gmail.com>
2017-09-14 18:08:04 -0400

    Write out owned_post method, call a before_action with it, and also specify authenticate_user crud actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -57,4 +58,10 @@ class PostsController < ApplicationController
     params.require(:post).permit(:caption, :image)
   end
 
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
+  end
 end
