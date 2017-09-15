6e90373066cfe16fbedc98a5c172569ce629c444
Allan James Lapid <ajLapid718@gmail.com>
2017-09-15 14:17:49 -0400

    Rough draft of comments controller CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_post
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+
+    if @comment.save
+      redirect_to @post
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
