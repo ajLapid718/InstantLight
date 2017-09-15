476d66b202d7ff42a8a98cec030fb76dd46e4b73
Allan James Lapid <ajLapid718@gmail.com>
2017-09-15 14:23:34 -0400

    Fill out comment crud properly

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,16 @@
 class CommentsController < ApplicationController
-  before_action :find_post
+  before_action :find_post # The before_action invokes the private method #find_post to ensure that we are creating a comment on the porper corresponding post
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
+    @comment = @post.comments.build(comment_params) # @post.comments.build(comment_params) makes use of the has_many relationship that the post model has and then builds a comment there, with the post_id assigned to the comment upon creation
+    @comment.user_id = current_user.id # set the foreign key (user_id) of the comment to the current user's id so that the comment belongs to a user
 
     if @comment.save
+      flash[:success] = "You commented the hell out of that post!"
       redirect_to @post
+    else
+      flash[:alert] = "Check the comment form, something went horribly wrong!"
+      render root_path
     end
   end
 
