4c791972582d1ac3b7917e641ef68194d464e43f
Allan James Lapid <ajLapid718@gmail.com>
2017-09-26 00:50:46 -0400

    Add index action to comments controller, only requesting the applications layout view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,14 @@
 class CommentsController < ApplicationController
   before_action :find_post # The before_action invokes the private method #find_post to ensure that we are creating a comment on the porper corresponding post
 
+  def index
+    @comments = @post.comments.order("created_at ASC")
+
+    respond_to do |format|
+      format.html { render layout: !request.xhr? }
+    end
+  end
+
   def create
     @comment = @post.comments.build(comment_params) # @post.comments.build(comment_params) makes use of the has_many relationship that the post model has and then builds a comment there, with the post_id assigned to the comment upon creation
     @comment.user_id = current_user.id # set the foreign key (user_id) of the comment to the current user's id so that the comment belongs to a user
