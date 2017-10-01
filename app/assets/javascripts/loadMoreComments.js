5ff895e63998e055f02cd731b84138afda4ac7ec
Allan James Lapid <ajLapid718@gmail.com>
2017-10-01 02:21:04 -0400

    Add event_default I was missing

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -3,6 +3,7 @@ Append.open = false;
 function ClickableCommentsLink(){
   $('.more-comments').click( function() {
     $(this).on('ajax:success', function(event) {
+      event.preventDefault();
       var postId = $(this).data("post-id");
       $("#comments_" + postId).html(event.detail[2].responseText);
       $("#comments-paginator-" + postId).html("<a id='more-comments' data-post-id=" + postId + "data-type='html' data-remote='true' href='/posts/" + postId + "/comments>show more comments</a>");
