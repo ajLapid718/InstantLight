3e2038e73a4b84920ae96824aef57f16679dcf88
Allan James Lapid <ajLapid718@gmail.com>
2017-09-30 23:15:07 -0400

    Slightly better functionality. Now just need to change the path so that we do not have to refresh to like or unlike

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,13 +8,14 @@ module PostsHelper
     end
   end
 
-  def liked_post(post)
-    if post.liked_by(current_user)
-      return 'glyphicon-heart'
-    else
-      return 'glyphicon-heart-empty'
-    end
-  end
+  # def liked_post(post)
+  #   if post.liked_by(current_user)
+  #     return 'glyphicon-heart'
+  #   end
+  #   if !post.liked_by(current_user)
+  #     return 'glyphicon-heart-empty'
+  #   end
+  # end
 
   private
 
