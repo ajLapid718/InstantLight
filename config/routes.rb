8478609a24045a545a6047d30e2db61926b8cb6a
Allan James Lapid <ajLapid718@gmail.com>
2017-10-03 00:31:49 -0400

    Run rails generate controller Relationships follow_user unfollow_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'relationships/follow_user'
+
+  get 'relationships/unfollow_user'
+
   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   get 'notifications', to: 'notifications#index'
 
