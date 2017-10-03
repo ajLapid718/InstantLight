3ec99a768d0c1adc501917fe925cdeaeb2cea190
Allan James Lapid <ajLapid718@gmail.com>
2017-10-03 00:14:12 -0400

    Run rails generate model Follow...establish belongs_to and has_many relationships...as well as has_many throughs

---
+++ app/models/follow.rb
@@ -0,0 +1,11 @@
+class Follow < ApplicationRecord
+  belongs_to :follower,
+    primary_key: :id,
+    foreign_key: :follower_id,
+    class_name: 'User'
+
+  belongs_to :following,
+    primary_key: :id,
+    foreign_key: :following_id
+    class_name: 'User'
+end
