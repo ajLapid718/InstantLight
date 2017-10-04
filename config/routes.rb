b8acac1e92b986d348ef58f213ab765d56c92879
Allan James Lapid <ajLapid718@gmail.com>
2017-10-03 22:07:22 -0400

    Configure custom route for browse action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'browse', to: 'posts#browse', as: :browse_posts
+
   post ':username/follow_user', to: 'relationships#follow_user', as: :follow_user
   post ':username/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 
